e4cb6160a345473a7550ff947ea7fd0a3ff146e5
eagleyinfz <eagle.yinfz@gmail.com>
2018-12-23 12:03:46 +0800

    enable discovery client

--- src/main/java/com/accenture/micropaas/RestProducerApplication.java
+++ src/main/java/com/accenture/micropaas/RestProducerApplication.java
@@ -3,14 +3,14 @@ package com.accenture.micropaas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrix
 @EnableCircuitBreaker
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableTurbine
 public class RestProducerApplication {
     public static void main(String[] args) {
